044b716102bd1db192d2701013faa31945814afe
PaulStoffregen <paul@pjrc.com>
2014-06-08 15:34:11 -0700

    Fix for bitbang I/O at higher clock speeds

--- pinouts/pin_functions.h
+++ pinouts/pin_functions.h
@@ -8,7 +8,11 @@
   #define output_pin(ddr, pin)        ddr |= _BV(pin)
   #define pullup_pin(ddr, port, pin)  ddr &= ~_BV(pin); port |= _BV(pin)
 #elif defined(TEENSYDUINO)
+  #if F_CPU > 48000000
+  #define pulse_pin(port, pin)        digitalWriteFast(pin, HIGH); asm("nop"); digitalWriteFast(pin, LOW)
+  #else
   #define pulse_pin(port, pin)        digitalWriteFast(pin, HIGH); digitalWriteFast(pin, LOW)
+  #endif
   #define set_pin(port, pin)          digitalWriteFast(pin, HIGH)
   #define clear_pin(port, pin)        digitalWriteFast(pin, LOW)
   #define output_pin(ddr, pin)        pinMode(pin, OUTPUT)
