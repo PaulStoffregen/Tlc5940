93235a0b7f1227f4109fdf6730f854cf7930ab2d
Adam Phelps <amp@opendns.com>
2016-08-23 09:13:19 -0700

    Allow NUM_TLCS to be specified with compile flags

--- tlc_config.h
+++ tlc_config.h
@@ -52,7 +52,9 @@
     of the first TLC to the SIN (TLC pin 26) of the next.  The rest of the pins
     are attached normally.
     \note Each TLC needs it's own IREF resistor */
+#ifndef NUM_TLCS
 #define NUM_TLCS    1
+#endif
 
 /** Determines how data should be transfered to the TLCs.  Bit-banging can use
     any two i/o pins, but the hardware SPI is faster.
